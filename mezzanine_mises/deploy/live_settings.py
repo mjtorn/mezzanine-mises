47de0952bdcb139fb808af48f90231553077d476
Markus Törnqvist <mjt@fadconsulting.com>
2012-10-27 23:06:53 +0300

    Settings for going live

--- mezzanine_mises/deploy/live_settings.py
+++ mezzanine_mises/deploy/live_settings.py
@@ -2,29 +2,36 @@
 DATABASES = {
     "default": {
         # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.sqlite3",
         # DB name or path to database file if using sqlite3.
         "NAME": "%(proj_name)s",
         # Not used with sqlite3.
-        "USER": "%(proj_name)s",
+        "USER": "",
         # Not used with sqlite3.
-        "PASSWORD": "%(db_pass)s",
+        "PASSWORD": "",
         # Set to empty string for localhost. Not used with sqlite3.
         "HOST": "127.0.0.1",
         # Set to empty string for default. Not used with sqlite3.
         "PORT": "",
+    },
+    'django_mises': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/var/www/mises.fi/mises.db',
     }
 }
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTOCOL", "https")
 
-CACHE_MIDDLEWARE_SECONDS = 60
+DEFAULT_FROM_ADDRESS = 'staff@mises.fi'
 
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.memcached.MemcachedCache",
-        "LOCATION": "127.0.0.1:11211",
-    }
-}
+TIME_ZONE = 'Europe/Helsinki'
 
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+USE_L10N = True
+LANGUAGE_CODE = 'fi'
+
+DATE_FORMAT = 'l j.m.Y.'
+TIME_FORMAT = 'h:i'
+DATETIME_FORMAT = 'j.m.Y. h:i'
+
+# EOF
+
